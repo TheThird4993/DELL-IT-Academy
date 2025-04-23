e79b3db03681f0131969275dcbf2fe9361c9ad4e
Rodrigo Presa Pacheco <rodrigopresapacheco@gmail.com>
2025-04-23 01:50:53 -0300

    .

--- main.c
+++ main.c
@@ -11,6 +11,7 @@
 #define TAM_MAX 101							// 100 caracteres + "\0"
 #define LINHAS 2
 #define TAM 7
+#define EVENTS 5
 
 typedef struct Empresa Empresa;
 
@@ -19,6 +20,7 @@ struct Empresa {
 	unsigned int pts;
 	int ano_f, battle_id;
 	uint8_t eventos;
+	int vet_eventos[EVENTS];
 };
 
 int pareamento(Empresa*, int, int*);
@@ -35,9 +37,12 @@ int evento(int, int, Empresa*);
 
 int* batalha(Empresa*, int);
 
+int compararPontuacao(const void*, const void*);
+
 int main() {
 	Empresa* empresas;
 	unsigned int num_empresas = 0, rodadas = 1;
+	int* perdedores;
 
 	srand((unsigned)time(NULL));
 
@@ -48,7 +53,7 @@ int main() {
 		else break;
 	}
 
-	empresas = (Empresa*)malloc(sizeof(Empresa)*num_empresas);
+	empresas = (Empresa*)malloc(sizeof(Empresa) * num_empresas);
 
 	for (int i = 0; i < num_empresas; i++) {
 		printf("\nDigite o nome da empresa: %d/%d\n", i + 1, num_empresas);
@@ -64,21 +69,54 @@ int main() {
 			if (empresas[i].ano_f < 0) printf("Ano invalido\n");
 			else break;
 		}
+		for (int j = 0; j < EVENTS; j++) {
+			empresas[i].vet_eventos[j] = 0;
+		}
 		empresas[i].pts = 70;
 	}
 
-	batalha(empresas, num_empresas);
+	perdedores = (int*)malloc(sizeof(int) * TAM);
+	memcpy(perdedores, batalha(empresas, num_empresas), TAM * sizeof(int));
 
+	int vencedor = -1;
+	for (int i = 0; i < num_empresas; i++) {
+		int encontrado = 0;
+		for (int j = 0; j < TAM; j++) {
+			if (perdedores[j] == i) {
+				encontrado = 1;
+				break;
+			}
+		}
+		if (!encontrado) {
+			vencedor = i;
+			break;
+		}
+	}
+
+	printf("\nVencedor:\n1 lugar: %s (Pontuacao: %d)\n%s\n\n", empresas[vencedor].nome, empresas[vencedor].pts, empresas[vencedor].slogan);
+
+	qsort(empresas, num_empresas, sizeof(Empresa), compararPontuacao);
+
+	for (int i = 1; i < num_empresas; i++) {
+		printf("%d lugar: %s (Pontuacao: %d)\n", i + 1, empresas[i].nome, empresas[i].pts);
+	}
+
+	printf("\n\nEstatisticas:\n");
+	for (int i = 0; i < num_empresas; i++) {
+		printf("startup %s:   pitches: %d, bugs: %d, trações: %d, investidores irritados: %d, fake news: %d\n", empresas[i].nome, empresas[i].vet_eventos[0], empresas[i].vet_eventos[1], empresas[i].vet_eventos[2], empresas[i].vet_eventos[3], empresas[i].vet_eventos[4]);
+	}
+
+	free(perdedores);
 	free(empresas);
-	printf("isover\n");
+	printf("\nisover\n");
 	return 0;
 }
 
-int pareamento(Empresa* empresas, int quant, int* vet_perd){
+int pareamento(Empresa* empresas, int quant, int* vet_perd) {
 	int* vet;
 	int vet_perdedores[TAM];
 	int num_duplas = 0, eliminados = 0;
-	int vet_teste[TAM+1] = {0};
+	int vet_teste[TAM + 1] = { 0 };
 
 	for (int i = 0; i < TAM; i++) {
 		vet_perdedores[i] = vet_perd[i];
@@ -88,17 +126,18 @@ int pareamento(Empresa* empresas, int quant, int* vet_perd){
 		empresas[i].battle_id = -1;
 		vet_teste[i] = -1;
 	}
-	
+
 	for (int i = 0; i < TAM; i++) {
 		if (vet_perdedores[i] != -1) {
 			eliminados++;
 		}
 	}
 
-	if (eliminados == 7) return 0;
-
 	num_duplas = (quant - eliminados) >> 1;
-	vet = (int*)malloc(sizeof(int)*num_duplas);
+
+	if (!num_duplas) return 0;
+
+	vet = (int*)malloc(sizeof(int) * num_duplas);
 
 	for (int j = 0; j < num_duplas; j++) {
 		vet[j] = 2;
@@ -110,7 +149,7 @@ int pareamento(Empresa* empresas, int quant, int* vet_perd){
 			if (j == vet_perdedores[k]) {
 				j++;
 				vet_perdedores[k] = -1;
-				k = 0;
+				k = -1;
 			}
 		}
 		if (vet[var]) {
@@ -157,16 +196,16 @@ int** funcMatriz(int num_bat, Empresa* emp, int num_emp) {
 		if (mat[0][var] == -1) mat[0][var] = i;
 		else mat[1][var] = i;
 	}
-	
+
 	return mat;
 }
 
 void sharkFight(Empresa* empresas, int emp_a, int emp_b) {
 	switch ((rand() % 2) + 1) {
-		case 1: empresas[emp_a].pts += 2;
-			break;
-		case 2: empresas[emp_b].pts += 2;
-			break;
+	case 1: empresas[emp_a].pts += 2;
+		break;
+	case 2: empresas[emp_b].pts += 2;
+		break;
 	};
 	return;
 }
@@ -179,12 +218,12 @@ void liberarMatriz(int** matriz) {
 }
 
 int rodada(int emp_1, int emp_2, Empresa* empresas) {
-	int sel_e = 0, rodadas = 1; 
+	int sel_e = 0, rodadas = 1;
 
 	for (int var = emp_1, i = 0, j = 0; i != 1;) {
-		printf("Rodada %d\n", rodadas++);
-		printf("\n| Empresas:  |       %s      | vs |      %s       |\n", empresas[emp_1].nome, empresas[emp_2].nome);
-		printf("| Pontuacao: |	     %d      |    |      %d 	  |\n\n", empresas[emp_1].pts, empresas[emp_2].pts);
+		printf("Batalha %d\n", rodadas++);
+		printf("\n| Empresas:  | %s | vs | %s |\n", empresas[emp_1].nome, empresas[emp_2].nome);
+		printf("| Pontuacao: |   %d   |    |   %d   |\n\n", empresas[emp_1].pts, empresas[emp_2].pts);
 		printf("Digite o evento desejado para a startup %s\n", empresas[var].nome);
 		printf("[0] Nada(Proxima startup): +0 pontos\n");
 		printf("[1] Pitch convincente: +6 pontos\n");
@@ -208,15 +247,15 @@ int rodada(int emp_1, int emp_2, Empresa* empresas) {
 		}
 	}
 
-	if(empresas[emp_1].pts == empresas[emp_2].pts) sharkFight(empresas, emp_1, emp_2);
+	if (empresas[emp_1].pts == empresas[emp_2].pts) sharkFight(empresas, emp_1, emp_2);
 
-	if (empresas[emp_1].pts > empresas[emp_2].pts) { 
-		printf("\n%s venceu a rodada!\n +30 pontos\n\n", empresas[emp_1].nome);
+	if (empresas[emp_1].pts > empresas[emp_2].pts) {
+		printf("\n%s venceu a batalha!\n +30 pontos\n\n", empresas[emp_1].nome);
 		empresas[emp_1].pts += 30;
 		return emp_2;
 	}
 	else if (empresas[emp_2].pts > empresas[emp_1].pts) {
-		printf("\n%s venceu a rodada!\n +30 pontos\n\n", empresas[emp_2].nome);
+		printf("\n%s venceu a batalha!\n +30 pontos\n\n", empresas[emp_2].nome);
 		empresas[emp_2].pts += 30;
 		return emp_1;
 	}
@@ -231,6 +270,7 @@ int evento(int num, int num_emp, Empresa* emp) {
 			printf("A startup %s fez um pitch convincente!\n +6 pontos\n\n", emp[num_emp].nome);
 			emp[num_emp].pts += 6;
 			emp[num_emp].eventos &= ~1;
+			emp[num_emp].vet_eventos[0]++;
 		}
 		else return 0;
 	}
@@ -241,6 +281,7 @@ int evento(int num, int num_emp, Empresa* emp) {
 			printf("A startup %s apresentou um produto com bugs!\n -4 pontos\n\n", emp[num_emp].nome);
 			emp[num_emp].pts -= 4;
 			emp[num_emp].eventos &= ~2;
+			emp[num_emp].vet_eventos[1]++;
 		}
 		else return 0;
 	}
@@ -251,55 +292,58 @@ int evento(int num, int num_emp, Empresa* emp) {
 			printf("A startup %s teve uma boa tracao de usuarios!\n +3 pontos\n\n", emp[num_emp].nome);
 			emp[num_emp].pts += 3;
 			emp[num_emp].eventos &= ~4;
+			emp[num_emp].vet_eventos[2]++;
 		}
 		else return 0;
 	}
-		break;
+		  break;
 
 	case 4: {
 		if (emp[num_emp].eventos & 8) {
 			printf("A startup %s fez um investidor ficar irritado!\n -6 pontos\n\n", emp[num_emp].nome);
 			emp[num_emp].pts -= 6;
 			emp[num_emp].eventos &= ~8;
+			emp[num_emp].vet_eventos[3]++;
 		}
 		else return 0;
 	}
-		break;
+		  break;
 
 	case 5: {
 		if (emp[num_emp].eventos & 16) {
 			printf("A startup %s apresentou fake news no pitch!\n -8 pontos\n\n", emp[num_emp].nome);
 			emp[num_emp].pts -= 8;
 			emp[num_emp].eventos &= ~16;
+			emp[num_emp].vet_eventos[4]++;
 		}
 		else return 0;
 	}
-		break;
+		  break;
 	}
 	return 1;
 }
 
-int* batalha(Empresa* emp, int num_empresas) {
+int* batalha(Empresa* empresas, int num_empresas) {
 	int** matriz_batalhas;
-	int sel = 0, cont = 1;
+	int sel = 0, cont_bat = 1, cont = 0, indice = 0;
 	static int vet_cont = 0, teste = 0;
-	int um_vencedor = 0, num_batalhas = 0;
+	int um_vencedor = 0, num_batalhas = 0, num_sel = 0;
 	int perdedor[TAM] = { 0 };
-	Empresa* empresas;
+	//Empresa* empresas;
 
 	teste = num_empresas - 1;
 
 	for (int i = 0; i < TAM; i++) perdedor[i] = -1;
 
 	while (1) {
-		empresas = (Empresa*)malloc(num_empresas * sizeof(Empresa));
-		memcpy(empresas, emp, num_empresas * sizeof(Empresa));
+		//empresas = (Empresa*)malloc(num_empresas * sizeof(Empresa));
+		//memcpy(empresas, emp, num_empresas * sizeof(Empresa));
 		num_batalhas = pareamento(empresas, num_empresas, perdedor);
 		matriz_batalhas = funcMatriz(num_batalhas, empresas, num_empresas);
 
 		if (!num_batalhas) break;
 
-		printf("\nBatalha %d", cont++);
+		printf("\nRodada %d", cont_bat++);
 
 		um_vencedor = 0;
 
@@ -307,62 +351,86 @@ int* batalha(Empresa* emp, int num_empresas) {
 
 			printf("\nDigite o numero da batalha que deseja administrar: \n");
 
-			printf("\n");
+			/*printf("\n");
 			for (int k = 0; k < num_batalhas; k++) printf("elemento [0][%d] da matriz: %d\n", k, matriz_batalhas[0][k]);
 			printf("\n");
 			printf("\n");
 			for (int k = 0; k < num_batalhas; k++) printf("elemento [1][%d] da matriz: %d\n", k, matriz_batalhas[1][k]);
-			printf("\n");
+			printf("\n");*/
+
+			num_sel = 0;
 
 			for (int i = 0, j = 1; i < num_batalhas; i++, j++) {
-				while(matriz_batalhas[0][i] == -1) i++;
+				while (matriz_batalhas[0][i] == -1) i++;
 				if ((i + 1) > num_batalhas) break;
 				printf("[%d] batalha %s vs %s\n", j, empresas[matriz_batalhas[0][i]].nome, empresas[matriz_batalhas[1][i]].nome);
-				
+				num_sel++;
 			}
 
 			while (1) {
 				scanf("%d", &sel);
-				printf("\n\nvalor: %d\n", matriz_batalhas[0][sel - 1]);
+				//printf("\n\nvalor: %d\n", matriz_batalhas[0][sel - 1]);
 
-				while (matriz_batalhas[0][sel - 1] == -1) sel++;
+				for (indice = 0, cont = 0; indice < num_batalhas; indice++) {
+					if (matriz_batalhas[0][indice] != -1) cont++;
+					if (cont == sel) break;
+					//printf("\nindice: %d\n", indice);
+				}
 
-				printf("\nvalor: %d\n\n", matriz_batalhas[0][sel - 1]);
+				//printf("\nindice: %d\n", indice);
+				//printf("\ncont: %d\n", cont);
 
-				if (sel <= 0 || sel > 4) printf("Selecao invalida\n");
+
+				//printf("\nvalor: %d\n\n", matriz_batalhas[0][indice]);
+
+				if (sel <= 0 || sel > num_sel) printf("Selecao invalida\n");
 				else break;
 			}
 
 			for (int i = 0; i < num_empresas; i++) empresas[i].eventos = 31; /// 0001 1111 -> flags para eventos
 
-			perdedor[vet_cont++] = rodada(matriz_batalhas[0][sel - 1], matriz_batalhas[1][sel - 1], empresas);
+			perdedor[vet_cont++] = rodada(matriz_batalhas[0][indice], matriz_batalhas[1][indice], empresas);
 
-			for (int i = 0; i < 2; i++) matriz_batalhas[i][sel - 1] = -1;
+			for (int i = 0; i < 2; i++) matriz_batalhas[i][indice] = -1;
 
-			printf("\n\nperdedores: ");
-			for (int i = 0; i < vet_cont; i++) printf("%d ", perdedor[i]);
-			printf("\n\n");
+			//printf("\n\nperdedores: ");
+			//for (int i = 0; i < vet_cont; i++) printf("%d ", perdedor[i]);
+			//printf("\n\n");
 
-			if (cont == 2) {
-				for (int i = 0; i < ((TAM+1) >> 1); i++) {
+			if (cont_bat == 2) {
+				for (int i = 0; i < (num_empresas >> 1); i++) {
 					um_vencedor = 1;
 					if (perdedor[i] == -1) um_vencedor = 0;
 				}
 			}
-			else {
-				for (int i = ((TAM + 1) >> 1); i < TAM-1; i++) {
+			else if (num_empresas == 8 || num_empresas == 6) {
+				for (int i = 0; i < num_empresas - 2; i++) {
 					um_vencedor = 1;
 					if (perdedor[i] == -1) um_vencedor = 0;
 				}
 			}
-			printf("\n\ncont: %d\n", cont);
-			printf("um_vencedor %d:\n\n ", um_vencedor);
+			else if (num_empresas == 4) {
+				for (int i = 0; i <= num_empresas - 2; i++) {
+					um_vencedor = 1;
+					if (perdedor[i] == -1) um_vencedor = 0;
+				}
+			}
+			//printf("\n\ncont: %d\n", cont_bat);
+			//printf("um_vencedor %d:\n\n ", um_vencedor);
 		}
 
+
 		liberarMatriz(matriz_batalhas);
-		free(empresas);
 	}
+
 	liberarMatriz(matriz_batalhas);
-	free(empresas);
 	return perdedor;
+}
+
+int compararPontuacao(const void* a, const void* b) {
+	Empresa* empresaA = (Empresa*)a;
+	Empresa* empresaB = (Empresa*)b;
+
+	// ordena em ordem decrescente de pontuação
+	return (empresaB->pts - empresaA->pts);
 }
